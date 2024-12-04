1a78f71b19b69a36826e7fed76265012ed235fc3
Sahil-047 <sahil_gcse2022@msit.edu.in>
2024-12-04 20:53:25 +0530

    First push for the notion

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Notion",
+  description: "A connected workspace where better, faster experiences are provided",
+  icons: {
+    icon: [
+      {
+        media: "{prefer-color-scheme: light}",
+        url: "/notionlight.webp",
+        href: "/notionlight.webp",
+      },
+      {
+        media: "{prefer-color-scheme: dark}",
+        url: "/Notion-logo.svg.png",
+        href: "/Notion-logo.svg.png",
+      },
+    ]
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ConvexClientProvider>
+          <ThemeProvider attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="notion-theme-2">
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
